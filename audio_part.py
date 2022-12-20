f6550af7438b3fb40a2c952adcad902b58731363
Jeevan-Rai <jeevanraisonangeri@gmail.com>
2022-12-20 12:32:23 +0530

    Update audio_part.py

--- audio_part.py
+++ audio_part.py
@@ -5,6 +5,8 @@ import time
 import threading
 import os
 
+# hello there
+
 def read_audio(stream, filename):
     chunk = 1024  # Record in chunks of 1024 samples
     sample_format = pyaudio.paInt16  # 16 bits per sample
